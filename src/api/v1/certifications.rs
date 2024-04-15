8356ae4e2881db365dbe995e4f5b15fa5189729b
Jacob Maizel <maizeljacob@gmail.com>
2024-04-15 17:41:34 -0400

    clear certifications endpoint

--- src/api/v1/certifications.rs
+++ src/api/v1/certifications.rs
@@ -2,7 +2,7 @@ use crate::{
     db::models::certification::{Certification, NewCertification},
     error::api_error,
     server::AppState,
-    types,
+    types::{self, AppResult},
     util::extractors::{JsonExtractor, QueryHmExt, UserIdExtractor},
 };
 use axum::{extract::State, routing::get, Json};
@@ -10,7 +10,12 @@ use diesel::{insert_into, prelude::*};
 use std::sync::Arc;
 
 pub fn certification_routes() -> axum::Router<Arc<AppState>> {
-    axum::Router::new().route("/", get(get_certifications).post(create_certification))
+    axum::Router::new().route(
+        "/",
+        get(get_certifications)
+            .post(create_certification)
+            .delete(delete_certifications),
+    )
 }
 
 async fn get_certifications(
@@ -50,6 +55,21 @@ async fn create_certification(
 ) -> types::DBResult<Certification> {
     use crate::schema::certifications::dsl::*;
 
+    // NOTE: This needs to be properly handled on frontend, but for now
+    // I am just going to delete any name dupes so I can do a simple POST to
+    // handle ALL changes in cert list
+    //
+    let mut conn = state.db_pool.get_conn();
+
+    let existing_name = certifications
+        .filter(name.eq(body.name.clone()).and(user_id.eq(u_id)))
+        .select(id)
+        .first::<uuid::Uuid>(&mut conn);
+
+    if let Ok(existing_cert_id) = existing_name {
+        let _ = diesel::delete(certifications.filter(id.eq(existing_cert_id))).execute(&mut conn);
+    }
+
     let val = NewCertification {
         user_id: Some(u_id),
         ..body
@@ -62,4 +82,16 @@ async fn create_certification(
         .map_err(api_error)?;
 
     Ok(Json(cert))
+}
+
+async fn delete_certifications(
+    State(state): State<Arc<AppState>>,
+    UserIdExtractor(u_id): UserIdExtractor,
+) -> AppResult<Json<serde_json::Value>> {
+    use crate::schema::certifications::dsl::*;
+
+    let mut conn = state.db_pool.get_conn();
+    let res: usize = diesel::delete(certifications.filter(user_id.eq(u_id))).execute(&mut conn)?;
+
+    Ok(Json(serde_json::json!({"deleted": res})))
 }
