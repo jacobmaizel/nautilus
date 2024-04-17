d90a13d582b4fb003628fd4de938a513dd39a13e
Jacob Maizel <maizeljacob@gmail.com>
2024-04-17 18:10:14 -0400

    ci fix: add db url to migration run

--- scripts/run_migrations.sh
+++ scripts/run_migrations.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-until diesel migration run --locked-schema; do
+until diesel migration run --database-url postgresql://postgres:postgres@localhost:5432/postgres --locked-schema; do
   echo "Migrations failed, retrying in 5 seconds..."
   sleep 5
 done
